95c75674ca781a7453bfdbb2954e4846b8aa98ae
Bruno Wego <brunowego@gmail.com>
2019-09-24 09:45:21 -0300

    chore(docker): added docker registry env

--- mk.d/docker.mk
+++ mk.d/docker.mk
@@ -1,13 +1,14 @@
 SHELL := /bin/sh
 
 DOCKER_BUILD_OPTS ?=
+DOCKER_REGISTRY ?= docker.io
 DOCKER_REPO := brunowego
 
 .PHONY: docker/build
 docker/build:
 	@docker build $(DOCKER_BUILD_OPTS) \
-		-t $(DOCKER_REPO)/$(DOCKER_IMAGE):$(DOCKER_TAG) \
-		-t $(DOCKER_REPO)/$(DOCKER_IMAGE):latest .
+		-t $(DOCKER_REGISTRY)/$(DOCKER_REPO)/$(DOCKER_IMAGE):$(DOCKER_TAG) \
+		-t $(DOCKER_REGISTRY)/$(DOCKER_REPO)/$(DOCKER_IMAGE):latest .
 	@$(MAKE) docker/clean
 
 .PHONY: docker/push
